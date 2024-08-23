b48a1f160aa6b9dae084dbd3028831057c51473f
JhonatanSO <jhonerex2012@gmail.com>
2024-08-23 18:33:15 -0300

    feat: Creating Offer Component

---
+++ src/components/header/header-additional/offer/offer-a.tsx
@@ -0,0 +1,27 @@
+// "use client";
+
+import styled from "styled-components";
+
+const ContainerOffer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+
+  background-color: ${({ theme }) => theme.colors.attention};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.s};
+`;
+
+export default function Offer() {
+  return (
+    <ContainerOffer>
+      <a href="#">
+        12% OFF em TODO o Site + Frete GRÁTIS, cupom: PRIMEIRACOMPRA. Condições
+        em promoções
+      </a>
+    </ContainerOffer>
+  );
+}
