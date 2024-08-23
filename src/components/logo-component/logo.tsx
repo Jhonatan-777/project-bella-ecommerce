48da9a717c6756419c114f743bc80b1cc588e1b9
JhonatanSO <jhonerex2012@gmail.com>
2024-08-23 18:35:31 -0300

    feat: Creating logo component

--- src/components/logo-component/logo.tsx
+++ src/components/logo-component/logo.tsx
@@ -1,10 +1,31 @@
+// "use client";
+
 import styled from "styled-components";
 
-export const Logo = styled.div`
+import { Sansita_Swashed } from "next/font/google";
+import Link from "next/link";
+
+const sansita = Sansita_Swashed({
+  weight: ["700"],
+  subsets: ["latin"],
+});
+
+const LogoContainer = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.xl};
   color: ${({ theme }) => theme.colors.primaryColor010};
+  cursor: pointer;
 
   span {
     color: ${({ theme }) => theme.colors.secondaryColor};
   }
 `;
+
+export default function Logo() {
+  return (
+    <LogoContainer>
+      <Link href="/shop" className={sansita.className}>
+        Bell<span>a</span>
+      </Link>
+    </LogoContainer>
+  );
+}
