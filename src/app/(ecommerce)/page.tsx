e9e27426b0c8cb8af83d4680e1366fe3973b23b4
JhonatanSO <jhonerex2012@gmail.com>
2024-11-25 19:02:06 -0300

    fix: server client

--- src/app/(ecommerce)/page.tsx
+++ src/app/(ecommerce)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <BannerMain />
       <BannersSubMain />
       <DiversifiedTrack />
-      <SectionNews />
+      {/* <SectionNews /> */}
     </ContainerSections>
   );
 }
